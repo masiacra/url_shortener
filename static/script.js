f43e2923d998a2e4174f889f3b81af8180505abe
masiacra <masiacra@gmail.com>
2019-05-27 17:39:48 +0300

    Added some corrects to post handler and script.js file

--- static/script.js
+++ static/script.js
@@ -1,13 +1,45 @@
-document.forms[0].onsubmit = () => {
-	const xhr = new XMLHttpRequest();
-	const longUrl = document.forms[0].longUrl.value;
-	const body = JSON.stringify({ longUrl });
-	document.forms[0].longUrl.value = '';
-	xhr.open('POST', '/', true);
-	xhr.setRequestHeader('Content-type', 
-	'application/json; charset=utf-8');
-	xhr.send(body);
-	xhr.onload = () => { console.log(this.responseText) };
-	xhr.onerror = () => { console.log(this.status) };
-	return false;
-};
+const form = new class {
+	constructor(elem) {
+		this.elem = elem;
+		this.elem.onsubmit = () => { 
+			return false; 
+		};
+		this.elem.onclick = this._onClick.bind(this);
+	}
+	
+	_onClick() {
+		const xhr = new XMLHttpRequest();
+		const inp = this.elem.longUrl;
+		const longUrl = inp.value;
+		if (this.elem.classList.contains('alert')) {
+			this.elem.classList.remove('alert');
+		}
+		
+		xhr.open('POST', '/', true);
+		xhr.setRequestHeader('Content-type', 
+		'application/json; charset=utf-8');
+		xhr.send(JSON.stringify({ longUrl }));
+		xhr.onload = function(){ 
+			if (this.status === 200) {
+				console.log(this.responseText);
+				if (inp.classList.contains('alert')) {
+					inp.classList.remove('alert');
+				}
+				inp.value = '';
+			} else {
+				if (!inp.classList.contains('alert')) {
+					inp.classList.add('alert');
+				}
+			}
+		};
+		xhr.onerror = function() { 
+			console.log(this.status) 
+		};		
+	}
+	
+	
+	
+}(document.forms[0]);
+
+
+
